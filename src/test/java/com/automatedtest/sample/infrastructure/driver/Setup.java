bf372f87ea57e15320174df2b6c7b61bc49cf26c
laxmi <laxmi.singhal.contr@ironmountain.com>
2020-08-29 14:55:29 +0530

    edited set up

--- src/test/java/com/automatedtest/sample/infrastructure/driver/Setup.java
+++ src/test/java/com/automatedtest/sample/infrastructure/driver/Setup.java
@@ -45,12 +45,6 @@ public class Setup {
 	//		System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
 			System.setProperty("webdriver.chrome.driver", "src/main/java/drivers/chromedriver_v84_linux");
 
-			/*
-			 * chromedriver --user-data-dir=$HOME/.config/google-chrome
-			 * --app=https://userapi.dit03-insight-dev.com/oauth2/207931/SSO/login/insight?
-			 * RelayState=/keep
-			 */
-
 			DesiredCapabilities cap = new DesiredCapabilities();
 			cap.setJavascriptEnabled(true);
 
@@ -58,57 +52,10 @@ public class Setup {
 			options = new ChromeOptions();
 			options.addArguments("--headless");
 			options.addArguments("--no-sandbox");
-			
-		//	System.out.println("executing linux commands");
-		/*//	ExecuteShellCommandRuntimeExec("ls -lrt");
-			ExecuteShellCommandRuntimeExec("pwd");
-			ExecuteShellCommandRuntimeExec("cd /home/");
-			ExecuteShellCommandRuntimeExec("ls");
-		//	ExecuteShellCommandRuntimeExec("echo $HOME");
-		*/	// echo $HOME
-			// /home/practiceframework/
-			// /home/practiceframework/.config/google-chrome
-	//		options.addArguments("--user-data-dir='$HOME/.config/chromedriver'");
-
-			// options.addArguments("--disable-setuid-sandbox");
-			// options.addArguments("disable-gpu");
-			// options.addArguments("--test-type");
-			// options.addArguments("--window-size=1920,1080");
-			// options.addArguments("window-size=1920,1080");
-			// options.addArguments("start-maximized");
-			// options.addArguments("--start-maximized");
-			// options.addArguments("--disable-dev-shm-usage");
-			// options.addArguments("--acceptSslCerts=true");
-			// options.addArguments("--ignore-certificate-errors");
-			// options.addArguments("--remote-debugging-port=9515");
-			// options.addArguments("--no-first-run");
-			// options.addArguments("--disable-extensions");
-			// options.addArguments("--proxy-server='direct://'");
-			// options.addArguments("--proxy-bypass-list=*");
-			// options.addArguments("--disable-dev-shm-usage");
-
 			System.out.println("Options are set");
 
 			options.merge(cap);
-
-			/*
-			 * DesiredCapabilities capabilities = DesiredCapabilities.chrome(); //
-			 * capabilities.setCapability("browser_version", "76");
-			 * capabilities.setCapability(ChromeOptions.CAPABILITY, options);
-			 * capabilities.setCapability(CapabilityType.ACCEPT_SSL_CERTS, true);
-			 * capabilities.setCapability(CapabilityType.ACCEPT_INSECURE_CERTS, true);
-			 * capabilities.setCapability(CapabilityType.UNEXPECTED_ALERT_BEHAVIOUR,
-			 * UnexpectedAlertBehaviour.IGNORE);
-			 */
 			System.out.println("caps set");
-			driver = new ChromeDriver(cap);
-
-			/*
-			 * DesiredCapabilities capabilities = DesiredCapabilities.chrome();
-			 * capabilities.setCapability(ChromeOptions.CAPABILITY, options);
-			 * options.merge(capabilities);
-			 */
-			driver = new ChromeDriver(options);
 			// driver.manage().deleteAllCookies();
 			System.out.println("driver = " + driver);
 			driver = new ChromeDriver(options);
