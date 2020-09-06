bf85ab662c2f73721c650a97a83afee45a50a81d
laxmi <laxmi.singhal.contr@ironmountain.com>
2020-09-06 14:53:57 +0530

    added 85 version in file

--- src/test/java/com/automatedtest/sample/infrastructure/driver/Setup.java
+++ src/test/java/com/automatedtest/sample/infrastructure/driver/Setup.java
@@ -50,7 +50,7 @@ public class Setup {
 		case "chrome":
 			System.out.println("inside linux system");
 			// System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
-			System.setProperty("webdriver.chrome.driver", "src/main/java/drivers/chromedriver_v84_linux");
+			System.setProperty("webdriver.chrome.driver", "src/main/java/drivers/chromedriver_Linux_v85");
 	//		System.setProperty("webdriver.chrome.driver", "src/main/java/drivers/chromedriver_v76_linux");
 			DesiredCapabilities cap = new DesiredCapabilities();
 			cap.setJavascriptEnabled(true);
@@ -62,9 +62,7 @@ public class Setup {
 		
 			options.merge(cap);
 			driver = new ChromeDriver(options);
-			driver.get("http://www.gmail.com");
-		//	driver.get("https://userapi.dit03-insight-dev.com/oauth2/207931/SSO/login/insight?RelayState=/keep");
-		//	driver.get(url);
+		/*	driver.get("http://www.gmail.com");
 			Thread.sleep(15000);
 			System.out.println(driver.getTitle());
 			if(driver.getTitle().equals("Gmail")) {
@@ -76,8 +74,7 @@ public class Setup {
 				System.out.println("***********************************************");
 
 			}
-			System.out.println("New Chrome driver Initiated successfully in linux");
-			senEmailThroughVMLinux();
+*/			System.out.println("New Chrome driver Initiated successfully in linux");
 			break;
 		case "firefox":
 			driver = new FirefoxDriver();
@@ -88,51 +85,6 @@ public class Setup {
 		}
 	}
 
-	public boolean verifyLogin() {
-		try {
-		//	Thread.sleep(20000);
-			driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-			System.out.println("entering email");
-			driver.findElement(By.id("Email")).click();
-			driver.findElement(By.id("Email")).sendKeys("devopstestingpractice@gmail.com");
-		 	System.out.println("clicking Next");
-			driver.findElement(By.id("next")).click();
-			System.out.println("clicked on Next");
-			Thread.sleep(20000);
-			driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-			System.out.println("Entering password");
-			WebElement ele = driver.findElement(By.id("Passwd-hidden"));
-			JavascriptExecutor executor = (JavascriptExecutor)driver;
-			executor.executeScript("arguments[0].click();", ele);
-			driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-			executor.executeScript("document.getElementById('Passwd-hidden').value='Learn&Master@123';");
-			System.out.println("entered password");
-			System.out.println("clicking next again");
-			driver.findElement(By.id("next")).click();
-			System.out.println("clicked next again");
-			driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-			Thread.sleep(10000);
-			if (driver.getTitle().equals("Gmail")) {
-				System.out.println(driver.getTitle());
-				try {
-					if(driver.findElement(By.xpath("/html/body/div[7]/div[3]/div/div[1]/div[3]/header/div[2]/div[2]/div[2]/form/div/table/tbody/tr/td/table/tbody/tr/td/div/input[1]")).isDisplayed()) {
-						System.out.println("Search in gmail is displayed");
-					}
-				}catch (Exception e) {
-					System.out.println("Search in gmail is no displayed");
-					e.printStackTrace();
-				}
-				
-				return true;
-			}
-		} catch (Exception e) {
-			System.out.println(driver.getTitle());
-			e.printStackTrace();
-		}
-		System.out.println("title not matched");
-		System.out.println(driver.getTitle());
-	return false;
-	}
 
 	public void ExecuteShellCommandRuntimeExec(String cmd) {
 		try {
